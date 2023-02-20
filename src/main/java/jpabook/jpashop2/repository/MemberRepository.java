57ebf7d565f65601ccf320b307f0c3c45be52040
sunga <eert2260@naver.com>
2023-02-20 23:43:50 +0900

    회원 기능 테스트

--- src/main/java/jpabook/jpashop2/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop2/repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop2.repository;
 
 import jpabook.jpashop2.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -8,10 +9,10 @@ import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
 
-    @PersistenceContext
-    private EntityManager em; // 스프링이 EntityManager를 만들어서 주입
+    private final EntityManager em; // 스프링이 EntityManager를 만들어서 주입
 
     public void save(Member member){
         em.persist(member);
@@ -23,6 +24,12 @@ public class MemberRepository {
 
     public List<Member> findAll(){
         return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name){
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
                 .getResultList();
     }
 }
