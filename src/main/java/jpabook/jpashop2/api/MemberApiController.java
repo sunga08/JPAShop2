cbf1c5e9347a67c5a238067fcc1be81604f333ba
sunga <eert2260@naver.com>
2023-03-03 07:17:16 +0900

    활용2 - 회원 등록 API

---
+++ src/main/java/jpabook/jpashop2/api/MemberApiController.java
@@ -0,0 +1,33 @@
+package jpabook.jpashop2.api;
+
+import jpabook.jpashop2.domain.Member;
+import jpabook.jpashop2.service.MemberService;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/api/v1/members")
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member){ /* @RequestBody: json으로 온 body 데이터를 Member에 매핑해서 넣어줌 */
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @Data
+    static class CreateMemberResponse{
+        private Long id;
+
+        public CreateMemberResponse(Long id){
+            this.id = id;
+        }
+    }
+}
