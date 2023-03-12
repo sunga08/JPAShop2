e4a36caea798f13b337994ddc167c08d3fc62ecc
sunga <eert2260@naver.com>
2023-03-12 21:24:03 +0900

    활용2 - QueryDSL 소개

--- src/main/java/jpabook/jpashop2/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop2/repository/OrderRepository.java
@@ -1,6 +1,11 @@
 package jpabook.jpashop2.repository;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import jpabook.jpashop2.domain.Order;
+import jpabook.jpashop2.domain.OrderStatus;
+import jpabook.jpashop2.domain.QMember;
+import jpabook.jpashop2.domain.QOrder;
 import jpabook.jpashop2.repository.order.simplequery.OrderSimpleQueryDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -75,6 +80,37 @@ public class OrderRepository {
         }
 
         return query.getResultList();
+    }
+
+    public List<Order> findAllByQueryDsl(OrderSearch orderSearch) {
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QOrder order = QOrder.order;
+        QMember member = QMember.member;
+
+
+        return query
+                .select(order)
+                .from(order)
+                .join(order.member, member)
+                .where(statusEq(orderSearch.getOrderStatus()), nameLike(orderSearch.getMemberName()))
+                //.where(order.status.eq(orderSearch.getOrderStatus())) -> 정적쿼리
+                .limit(1000)
+                .fetch();
+    }
+
+    private BooleanExpression nameLike(String memberName) {
+        if (!StringUtils.hasText(memberName)) {
+            return null;
+        }
+
+        return QMember.member.name.like(memberName);
+    }
+
+    private BooleanExpression statusEq(OrderStatus statusCond) {
+        if(statusCond == null) {
+            return null;
+        }
+        return QOrder.order.status.eq(statusCond);
     }
 
     public List<Order> findAllWithMemberRepository(){
